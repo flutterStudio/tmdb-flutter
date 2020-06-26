906389d3fcd69df50dd9a7892e7d13683f3a46b8
sameer.ahmad1024@gmail.com <sameer.ahmad1024@gmail.com>
2020-06-26 13:33:56 +0300

    master: Add icons, Implement  class and add  Dark  theme colors.

---
+++ lib/common/settings.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class Settings {
+  // Dark theme colors.
+  static const Color COLOR_DARK_PRIMARY = Color(0XFF0C252E);
+  static const Color COLOR_DARK_SECONDARY = Color(0XFF09191F);
+  static const Color COLOR_DARK_HIGHLIGHT = Color(0XFF257088);
+  static const Color COLOR_DARK_SHADOW = Color(0XFF040F13);
+}
