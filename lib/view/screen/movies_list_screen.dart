cc971c95df09739456373afa934eb2393a6359ff
sameer.ahmad1024@gmail.com <sameer.ahmad1024@gmail.com>
2020-06-28 22:22:46 +0300

    TMDB_M_1 Implement static design of Movies List Screen.

---
+++ lib/view/screen/movies_list_screen.dart
@@ -0,0 +1,67 @@
+import 'package:TMDB_Mobile/common/settings.dart';
+import 'package:TMDB_Mobile/view/widget/item_movie_stacked_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MoviesListScreen extends StatefulWidget {
+  @override
+  _MoviesListScreenState createState() => _MoviesListScreenState();
+}
+
+class _MoviesListScreenState extends State<MoviesListScreen> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: NestedScrollView(
+            headerSliverBuilder: _headerSliverBuilder,
+            body: _sliverBuilderBody(context)),
+      );
+
+  List<Widget> _headerSliverBuilder(
+      BuildContext context, bool innerBoxScrolled) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return [
+      SliverAppBar(
+        floating: true,
+        snap: true,
+        backgroundColor: Settings.COLOR_DARK_SECONDARY,
+        centerTitle: true,
+        title: Text(
+          "Movies List",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              color: Settings.COLOR_DARK_TEXT,
+              fontWeight: FontWeight.w300,
+              fontSize: screenWidth * Settings.FONT_SIZE_LARGE),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+          ),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        actions: <Widget>[],
+      )
+    ];
+  }
+
+  Widget _sliverBuilderBody(BuildContext context) {
+    return Container(
+      color: Settings.COLOR_DARK_PRIMARY,
+      child: GridView.builder(
+          addAutomaticKeepAlives: true,
+          padding: EdgeInsets.all(0),
+          itemCount: 20,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, childAspectRatio: 0.8),
+          itemBuilder: (context, index) => MovieStackedView(
+                image: "assets/placeholders/poster.jpg",
+                width: MediaQuery.of(context).size.width * 0.5,
+                height: MediaQuery.of(context).size.height * 0.35,
+                highlight: index % 2 == 0
+                    ? Settings.COLOR_DARK_PRIMARY
+                    : Settings.COLOR_DARK_HIGHLIGHT,
+                name: "Dark",
+              )),
+    );
+  }
+}
---
+++ lib/view/widget/item_movie_stacked_view.dart
@@ -0,0 +1,83 @@
+import 'dart:ui';
+
+import 'package:TMDB_Mobile/common/settings.dart';
+import 'package:flutter/widgets.dart';
+
+class MovieStackedView extends StatelessWidget {
+  final double _width;
+  final double _height;
+  final String _image;
+  final String _name;
+  final Color _highlight;
+
+  MovieStackedView(
+      {double width, double height, String name, String image, Color highlight})
+      : _image = image,
+        _height = height,
+        _width = width,
+        _name = name,
+        _highlight = highlight,
+        assert(image != null),
+        assert(height != null),
+        assert(width != null),
+        assert(name != null);
+
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return Stack(children: [
+      Container(
+        width: _width,
+        height: _height,
+        decoration: BoxDecoration(
+            color: Settings.COLOR_DARK_SECONDARY,
+            image:
+                DecorationImage(fit: BoxFit.cover, image: AssetImage(_image)),
+            borderRadius: BorderRadius.circular(5)),
+      ),
+      Positioned(
+        bottom: 0,
+        width: _width,
+        height: _height * 0.25,
+        child: BackdropFilter(
+          filter: ImageFilter.blur(),
+          child: Container(
+            padding: EdgeInsets.symmetric(
+                vertical: _height * 0.02, horizontal: _width * 0.05),
+            width: _width,
+            height: _height * 0.25,
+            color: _highlight.withOpacity(0.7),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Text(_name,
+                    maxLines: 1,
+                    style: TextStyle(
+                        color: Settings.COLOR_DARK_TEXT,
+                        fontWeight: FontWeight.w300,
+                        fontSize: screenWidth * Settings.FONT_SIZE_MEDIUM)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Oct 10, 2012",
+                        style: TextStyle(
+                            color: Settings.COLOR_DARK_TEXT,
+                            fontWeight: FontWeight.w300,
+                            fontSize: screenWidth * Settings.FONT_SIZE_SMALL)),
+                    Text("Rating",
+                        style: TextStyle(
+                            color: Settings.COLOR_DARK_TEXT,
+                            fontWeight: FontWeight.w300,
+                            fontSize: screenWidth * Settings.FONT_SIZE_SMALL))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      )
+    ]);
+  }
+}
